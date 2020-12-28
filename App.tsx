7daf16d712c0fa0f92223581dafa97add0410800
elizabethhart <elizabethwhart@gmail.com>
2020-12-28 16:33:14 -0600

    Try out Text, Image, View

--- App.tsx
+++ App.tsx
@@ -1,20 +1,59 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { 
+  Button,
+  Image, 
+  SafeAreaView, 
+  StyleSheet, 
+  Text, 
+  TouchableHighlight
+} from 'react-native';
 
 export default function App() {
+
+  const handleTextPress = () => {
+    console.log("Hello World");
+  }
+
+  const handleImagePress = () => {
+    console.log("Hello World");
+  }
+
+  const handleButtonPress = () => {
+    console.log("Hello World");
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Hello World</Text>
+    <SafeAreaView style={styles.container}>
+      <Text numberOfLines={1} onPress={handleTextPress}>
+        Hello World
+      </Text>
+      <TouchableHighlight
+        onPress={handleImagePress}
+      >
+        <Image 
+          fadeDuration={1000}
+          source={{
+            width: 200,
+            height: 300,
+            uri: "https://picsum.photos/200/300"
+          }}
+        />
+      </TouchableHighlight>
+      <Button 
+        color="orange"
+        title="Click Me"
+        onPress={handleButtonPress}
+      />
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#5aad2f',
     alignItems: 'center',
     justifyContent: 'center',
   },
