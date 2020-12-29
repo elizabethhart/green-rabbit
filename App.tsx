22f6ec160d8b0e5640424a19b4902c89d44f24bb
elizabethhart <elizabethwhart@gmail.com>
2020-12-29 13:36:30 -0600

    Add hooks

--- App.tsx
+++ App.tsx
@@ -2,16 +2,20 @@ import React from 'react';
 import {
   Alert,
   Button,
+  Dimensions,
   Image,
   Platform,
   SafeAreaView,
   StatusBar,
   StyleSheet,
   Text,
-  TouchableHighlight
+  TouchableHighlight,
+  View
 } from 'react-native';
+import { useDeviceOrientation, useDimensions } from '@react-native-community/hooks';
 
 export default function App() {
+  const { landscape } = useDeviceOrientation();
 
   const handleTextPress = () => {
     console.log("Hello World");
@@ -33,34 +37,45 @@ export default function App() {
     Alert.prompt("Title", "Message", (text: string) => console.log(text));
   }
 
+  const content = () => {
+    return (
+      <>
+        <Text numberOfLines={1} onPress={handleTextPress}>
+            Hello World
+          </Text>
+          <TouchableHighlight
+            onPress={handleImagePress}
+          >
+            <Image 
+              fadeDuration={1000}
+              source={{
+                width: 200,
+                height: 300,
+                uri: "https://picsum.photos/200/300"
+              }}
+            />
+          </TouchableHighlight>
+          <Button 
+            color="orange"
+            title="Click Me"
+            onPress={handleButtonPress1}
+          />
+          <Button 
+            color="orange"
+            title="Click Me"
+            onPress={handleButtonPress2}
+          />
+      </>
+    );
+  }
+
   return (
     <SafeAreaView style={[styles.container, styles.other]}>
-      <Text numberOfLines={1} onPress={handleTextPress}>
-        Hello World
-      </Text>
-      <TouchableHighlight
-        onPress={handleImagePress}
-      >
-        <Image 
-          fadeDuration={1000}
-          source={{
-            width: 200,
-            height: 300,
-            uri: "https://picsum.photos/200/300"
-          }}
-        />
-      </TouchableHighlight>
-      <Button 
-        color="orange"
-        title="Click Me"
-        onPress={handleButtonPress1}
-      />
-      <Button 
-        color="orange"
-        title="Click Me"
-        onPress={handleButtonPress2}
-      />
-      <StatusBar style="auto" />
+      <View style={{ 
+        backgroundColor: 'blue', 
+        width: '100%', 
+        height: landscape ? '100%' : '30%', 
+      }}></View>
     </SafeAreaView>
   );
 }
